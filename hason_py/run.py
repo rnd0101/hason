bde9792478ec87511e6d7c61b5bc00c3c4bad1d6
Roman Suzi <roman.suzi@talentadore.com>
2021-04-18 08:44:05 +0300

    proto started. hello world

---
+++ hason_py/run.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""Run-time
+"""
+import argparse
+import json
+
+
+def println(data, env):
+    print(*[interpret(d, env) for d in data])
+
+
+builtin_functions = {
+    "print": println,
+}
+
+
+def interpret_list(data, env):
+    builtins = env.get("builtins", {})
+    func = builtins.get(data[0])
+    if func is not None:
+        return func(data[1:], env)
+    return data
+
+
+def interpret(data, env):
+    if not isinstance(data, dict) or data.get("") is None:
+        return data
+    if isinstance(data.get(""), list):
+        return interpret_list(data.get(""), env)
+    return data
+
+
+def main(filename):
+    with open(filename, 'rb') as json_file:
+        contents = json.load(json_file)
+    result = interpret(contents, {"builtins": builtin_functions})
+    print(result)
+
+
+if __name__ == '__main__':
+    import sys
+
+    parser = argparse.ArgumentParser(description="__doc__")
+    parser.add_argument('infile', nargs='?', type=str, default=sys.stdin)
+    args = parser.parse_args()
+    main(args.infile)
